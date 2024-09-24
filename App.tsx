83ccb1e1b2cf8f52d468f90d5138e7d716453747
Marcos Guibson <mguibtech@gmail.com>
2024-09-23 20:55:26 -0400

    [feat] - Adding item component and inital config bibliography itens

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {SafeAreaView} from 'react-native';
 import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): React.JSX.Element {
   return (
@@ -10,6 +11,8 @@ function App(): React.JSX.Element {
       <SafeAreaView>
         <Button title="Teste" mb="s10" preset="primary" disabled />
         <Button title="Teste" preset="outline" loading />
+        <Icon name="arrowRight" />
+        <Icon name="eyeOn" color="buttonPrimary" size={50} />
       </SafeAreaView>
     </ThemeProvider>
   );
