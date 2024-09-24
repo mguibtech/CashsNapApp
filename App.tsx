5d50dd3cf7614e3cb7da18cbccbe009d7cbc6724
Marcos Guibson <mguibtech@gmail.com>
2024-09-23 22:31:00 -0400

    [feat] - Adding textInput component

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Icon} from './src/components/Icon/Icon';
+import {TextInput} from './src/components/TextInput/TextInput';
 
 function App(): React.JSX.Element {
   return (
@@ -13,6 +14,11 @@ function App(): React.JSX.Element {
         <Button title="Teste" preset="outline" loading />
         <Icon name="arrowRight" />
         <Icon name="eyeOn" color="buttonPrimary" size={50} />
+        <TextInput
+          label="Senha"
+          errorMessage="teste"
+          rightComponent={<Icon name="arrowLeft" />}
+        />
       </SafeAreaView>
     </ThemeProvider>
   );
